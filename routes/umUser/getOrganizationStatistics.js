2fcf2f969af3343ea5a438e1dc81e9d142ca44c2
Hector Vigil <hector.vigil@ntsprint>
2021-09-14 08:30:45 -0400

    minor updatesz

--- routes/umUser/getOrganizationStatistics.js
+++ routes/umUser/getOrganizationStatistics.js
@@ -1,25 +1,11 @@
 const LocalStorage = require("node-localstorage").LocalStorage,
   localStorage = new LocalStorage("./scratch");
 
-const teachingCategory = [
-    "Asistente",
-    "Instructor",
-    "Titular",
-    "Auxiliar"
-  ];
+const teachingCategory = ["Asistente", "Instructor", "Titular", "Auxiliar"];
 
-const scientificCategory = [
-  "Master",
-  "Doctor"
-];
+const scientificCategory = ["Master", "Doctor"];
 
-const occupationalPosition = [
-  "T",
-  "CS",
-  "S",
-  "A",
-  "O"
-];
+const occupationalPosition = ["T", "CS", "S", "A", "O"];
 
 const contractType = [
   "Indeterminado",
@@ -28,87 +14,89 @@ const contractType = [
   "Jubilado",
   "V",
   "P",
-  "1"
-]
+  "1",
+];
 
 const belongToOrganization = (organizations, professor) => {
-  if (organizations[0] === 'root') {
+  if (organizations[0] === "root") {
     return true;
   }
-  const org = organizations.find(org => {
-    if (org === professor.unidad_organizativa)
-      return true;
+  const org = organizations.find((org) => {
+    if (org === professor.unidad_organizativa) return true;
   });
   return org;
 };
 
 module.exports = {
-  method: "get",
+  method: "post",
   path: "/organization-statistics",
   handler: async (ctx) => {
-
     const data = JSON.parse(localStorage.getItem("ASSET.json"));
     const total = {};
 
-
-    let { organizations } = ctx.request.query;
+    let { organizations } = ctx.request.body;
     console.log(organizations);
     organizations = JSON.parse(organizations);
 
-    data.forEach(professor => {
-
+    data.forEach((professor) => {
       if (belongToOrganization(organizations, professor)) {
-        teachingCategory.forEach(tc => {
+        teachingCategory.forEach((tc) => {
           if (!total[tc]) {
-            if (professor.categoria_docente === tc)
-                total[tc] = 1;
+            if (professor.categoria_docente === tc) total[tc] = 1;
+          } else {
+            if (professor.categoria_docente === tc) total[tc] += 1;
           }
-          else {
-            if (professor.categoria_docente === tc)
-                total[tc] += 1;
-          }
-          scientificCategory.forEach(sc => {
+          scientificCategory.forEach((sc) => {
             if (!total[`${tc} ${sc}`]) {
-                if (professor.categoria_docente === tc
-                  && professor.categoria_cientifica === sc)
-                    total[`${tc} ${sc}`] = 1;
-              }
-            else {
-              if (professor.categoria_docente === tc
-                && professor.categoria_cientifica === sc)
-                  total[`${tc} ${sc}`] += 1;
+              if (
+                professor.categoria_docente === tc &&
+                professor.categoria_cientifica === sc
+              )
+                total[`${tc} ${sc}`] = 1;
+            } else {
+              if (
+                professor.categoria_docente === tc &&
+                professor.categoria_cientifica === sc
+              )
+                total[`${tc} ${sc}`] += 1;
             }
-            occupationalPosition.forEach(op => {
+            occupationalPosition.forEach((op) => {
               if (!total[`${tc} ${sc} ${op}`]) {
-                if (professor.categoria_docente === tc
-                  && professor.categoria_cientifica === sc
-                  && professor.posicion_ocupacional === op)
-                    total[`${tc} ${sc} ${op}`] = 1;
+                if (
+                  professor.categoria_docente === tc &&
+                  professor.categoria_cientifica === sc &&
+                  professor.posicion_ocupacional === op
+                )
+                  total[`${tc} ${sc} ${op}`] = 1;
+              } else {
+                if (
+                  professor.categoria_docente === tc &&
+                  professor.categoria_cientifica === sc &&
+                  professor.posicion_ocupacional === op
+                )
+                  total[`${tc} ${sc} ${op}`] += 1;
               }
-              else {
-                if (professor.categoria_docente === tc
-                  && professor.categoria_cientifica === sc
-                  && professor.posicion_ocupacional === op)
-                    total[`${tc} ${sc} ${op}`] += 1;
-              }
-              contractType.forEach(ct => {
+              contractType.forEach((ct) => {
                 if (!total[`${tc} ${sc} ${op} ${ct}`]) {
-                  if (professor.categoria_docente === tc
-                    && professor.categoria_cientifica === sc
-                    && professor.posicion_ocupacional === op
-                    && professor.tipo_de_contrato === ct)
-                      total[`${tc} ${sc} ${op} ${ct}`] = 1;
-                }
-                else {
-                  if (professor.categoria_docente === tc
-                    && professor.categoria_cientifica === sc
-                    && professor.posicion_ocupacional === op
-                    && professor.tipo_de_contrato === ct)
-                      total[`${tc} ${sc} ${op} ${ct}`] += 1;
+                  if (
+                    professor.categoria_docente === tc &&
+                    professor.categoria_cientifica === sc &&
+                    professor.posicion_ocupacional === op &&
+                    professor.tipo_de_contrato === ct
+                  )
+                    total[`${tc} ${sc} ${op} ${ct}`] = 1;
+                } else {
+                  if (
+                    professor.categoria_docente === tc &&
+                    professor.categoria_cientifica === sc &&
+                    professor.posicion_ocupacional === op &&
+                    professor.tipo_de_contrato === ct
+                  )
+                    total[`${tc} ${sc} ${op} ${ct}`] += 1;
                 }
               });
             });
-          }) 
+          });
         });
       }
     });
@@ -116,6 +104,5 @@ module.exports = {
     ctx.body = total;
 
     ctx.status = 200;
-
-  }
+  },
 };
