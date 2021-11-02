a65161e4a6e96c8f9845f11b285127aa3297d349
Hector Vigil <hector.vigil@ntsprint>
2021-11-02 03:45:21 -0400

    add get professor query

---
+++ routes/umUser/getProfessors.js
@@ -0,0 +1,22 @@
+const LocalStorage = require("node-localstorage").LocalStorage,
+  localStorage = new LocalStorage("./scratch");
+
+module.exports = {
+  method: "get",
+  path: "/professors-list",
+  handler: async (ctx) => {
+    const data = JSON.parse(localStorage.getItem("ASSET.json"));
+    let professors = [];
+
+    data.forEach((professor) => {
+      professors.push({
+        nombre: professor.nombre,
+        expediente: professor.expediente,
+      });
+    });
+
+    ctx.body = professors;
+
+    ctx.status = 200;
+  },
+};
